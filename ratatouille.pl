f4a92eb5a7f3a6a759e1671a73d878f82ee77be5
vpennella <vpennella@gmail.com>
2021-08-20 20:29:32 -0300

    ejercicio ratatouille

--- ratatouille.pl
+++ ratatouille.pl
@@ -28,6 +28,72 @@ tieneTutor(linguini, Rata):- viveEn(Rata, Lugar),
     trabajaEn(linguini, Lugar).
 tieneTutor(skinner, amelie).
 
+%3
+esChef(Cocinero, Restaurante):- trabajaEn(Cocinero, Restaurante), 
+        cumpleCondicionesChef(Cocinero, Restaurante).
+
+cumpleCondicionesChef(Cocinero, Restaurante):-
+    forall(estaEnElMenu(Plato, Restaurante), cocinaBien(Cocinero, Plato)).
+cumpleCondicionesChef(Cocinero, _):- 
+    totalExperiencia(Cocinero, Total), Total > 20.
+
+totalExperiencia(Cocinero, Total):-
+    findall(Experiencia,sabeCocinar(Cocinero, _, Experiencia), ListaExperiencias), 
+    sumlist(ListaExperiencias, Total).
+
+%4
+encargada(Persona, Plato, Restaurante):- experienciaCocinero(Persona, Plato,  Restaurante, Experiencia),
+    forall(experienciaCocinero(_, Plato, Restaurante, OtraExperiencia), Experiencia >= OtraExperiencia).
+
+experienciaCocinero(Persona, Plato, Restaurante, Experiencia):-
+    trabajaEn(Persona, Restaurante),
+    sabeCocinar(Persona, Plato, Experiencia).
+plato(ensaladaRusa, entrada([papa, zanahoria, arvejas, huevo, mayonesa])).
+plato(bifeDeChorizo, principal(pure, 20)).
+plato(frutillasConCrema, postre(265)).
+plato(ensaladaDeFrutas, postre(50)).
+
+%5
+esSaludable(Comida):-plato(Comida, TipoComida), 
+    calorias(TipoComida, Calorias), Calorias < 75.
+
+calorias(entrada(ListaIngredientes), Calorias):-
+    length(ListaIngredientes, CantidadCalorias),
+    Calorias is CantidadCalorias * 15.
+calorias(principal(Guarnicion, Minutos), Calorias):-
+    caloriasGuarnicion(Guarnicion, CaleriasGuarnicion), 
+    Calorias is (Minutos * 5) + CaleriasGuarnicion.
+calorias(postre(Calorias), Calorias).
+
+caloriasGuarnicion(papasFritas, 50).
+caloriasGuarnicion(pure, 20).
+caloriasGuarnicion(ensalada, 0).
+
+%6
+reseniaPositiva(Critico, Restaurante):-restaurante(Restaurante),
+    not(viveEn(_, Restaurante)),
+    criterioCritico(Criterio, Restaurante).
+
+restaurante(Restaurante):- trabajaEn(_, Restaurante).
+
+criterioCritico(antonEgo, Restaurante):- esEspecialista(Restaurante, ratatouille).
+criterioCritico(cormillot, Restaurante):- forall(experienciaCocinero(_, Plato, Restaurante, _), esSaludable(Plato)).
+criterioCritico(martiniano,Restaurante):- esChef(Cocinero, Restaurante), 
+    not((esChef(OtroCocinero, Restaurante), Cocinero \= OtroCocinero)).
+
+esEspecialista(Restaurante, Comida):- 
+    forall(esChef(Cocinero, Restaurante),cocinaBien(Cocinero,Comida)).
+
+
+
+    
+
+
+
+
+
+
+
 
 
 
